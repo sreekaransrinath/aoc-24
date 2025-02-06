b74ab728d05337ba54965e6fcafca67e9a2d1a4e
Sreekaran <ss@sreekaran.com>
2025-02-06 18:32:23 -0500

    pt 2 attempt 1

--- python/2.py
+++ python/2.py
@@ -35,6 +35,36 @@ Analyze the unusual data from the engineers. How many reports are safe?
 
 from input_2 import input_str
 
+def generate_sublists(list: list) -> list:
+    sublists = []
+    for index in range(len(list)):
+        sublists.append(list[:index] + list[index+1:])
+    return sublists
+
+def is_safe(list: list):
+    is_broken = False
+    if list[1] > list[0]:
+        for i in range(len(list) - 1):
+            diff = list[i+1] - list[i]
+            if (diff > 3) or (diff < 1):
+                is_broken = True
+                break
+    
+    elif list[1] < list[0]:
+        for i in range(len(list) - 1):
+            diff = list[i] - list[i+1]
+            if (diff > 3) or (diff < 1):
+                is_broken = True
+                break
+    
+    else:
+        is_broken = True
+    
+    if is_broken == False:
+        return True
+    else:
+        return False
+
 def parse_input(input_str) -> list:
     output_nested_list = []
     for line in input_str.strip().split("\n"):
@@ -46,28 +76,51 @@ def count_safe(input_str: str) -> int:
     safe_count = 0
 
     for list in parse_input(input_str):
-        if list[1] > list[0]:
-            is_broken = False
-            for i in range(len(list) - 1):
-                diff = list[i+1] - list[i]
-                if (diff > 3) or (diff < 1):
-                    is_broken = True
-                    break
-            # print(list)
-        
-        elif list[1] < list[0]:
-            for i in range(len(list) - 1):
-                is_broken = False
-                diff = list[i] - list[i+1]
-                # print(diff)
-                if (diff > 3) or (diff < 1):
-                    is_broken = True
-                    break
-            # print(list)
-        
-        if is_broken == False:
+        if is_safe(list):
             safe_count += 1
 
     return safe_count
 
 print(count_safe(input_str))
+
+
+"""
+
+--- Part Two ---
+The engineers are surprised by the low number of safe reports until they realize they forgot to tell you about the Problem Dampener.
+
+The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single bad level in what would otherwise be a safe report. It's like the bad level never happened!
+
+Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
+
+More of the above example's reports are now safe:
+
+7 6 4 2 1: Safe without removing any level.
+1 2 7 8 9: Unsafe regardless of which level is removed.
+9 7 6 2 1: Unsafe regardless of which level is removed.
+1 3 2 4 5: Safe by removing the second level, 3.
+8 6 4 4 1: Safe by removing the third level, 4.
+1 3 6 7 9: Safe without removing any level.
+Thanks to the Problem Dampener, 4 reports are actually safe!
+
+Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe?
+
+"""
+
+def count_damped_safe(input_str: str) -> int:
+    safe_count = 0
+
+    for list in parse_input(input_str):
+        if is_safe(list):
+            safe_count += 1
+
+        else:
+            sublists = generate_sublists(list)
+            for sublist in sublists:
+                if is_safe(sublist):
+                    safe_count += 1
+                    break
+
+    return safe_count
+    
+print(count_damped_safe(input_str))
